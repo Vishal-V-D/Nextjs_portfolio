7278147a83dca4e586862d143aa27ce981f98416
Vishal-V-D <marvelhero60@gmail.com>
2024-11-22 13:55:26 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,31 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
+
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
+
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * The home page of the website, featuring a hero section, recent projects, client testimonials, experience, approach, and a footer.
+ * Conditionally renders a floating navbar based on whether the "RecentProjects" section is in view.
+ * @returns The JSX for the home page.
+ */
+/******  f585bba0-e841-46a2-a150-0a9814797c10  *******/
 const Home = () => {
-  const [showNav, setShowNav] = useState(false);
-
-  // Function to handle scroll and check when the "RecentProjects" section is in view
-  const handleScroll = () => {
-    const projectsSection = document.getElementById("projects");
-    if (projectsSection) {
-      const rect = projectsSection.getBoundingClientRect();
-      setShowNav(rect.top <= window.innerHeight && rect.bottom >= 0);
-    }
-  };
-
-  // Listen for scroll events
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    // Cleanup event listener on unmount
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* Conditionally render FloatingNav based on scroll */}
-        {showNav && <FloatingNav navItems={navItems} />}
+        <FloatingNav navItems={navItems} />
         <Hero />
-        {/**<Grid />**/}
+        {/** <Grid />**/}
         <RecentProjects />
-        {/**<Clients /> */}
+      {/**<Clients /> */}  
         <Experience />
         <Approach />
         <Footer />
